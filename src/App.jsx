01ee7a2af9248459eb9e4206e7a3282c858b3bc1
Yra <yurchik.yuri.ru@yandex.ru>
2025-07-17 20:07:24 +0500

    add file db.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,6 @@
 import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 
-const PRODUCTS_MOCK = [
-  {
-    id: "001",
-    name: "Телевизор",
-    price: 39900,
-  },
-  {
-    id: "002",
-    name: "Телефон",
-    price: 15500,
-  },
-  {
-    id: "003",
-    name: "Фен",
-    price: 1230,
-  },
-];
-
 function App() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -26,11 +8,7 @@ function App() {
   useEffect(() => {
     setIsLoading(true);
 
-    new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({ json: () => PRODUCTS_MOCK });
-      }, 3000);
-    })
+    fetch("http://localhost:3005/products")
       .then((loadedData) => loadedData.json())
       .then((loadedProducts) => {
         setProducts(loadedProducts);
